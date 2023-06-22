daa816b79245e4745466fb74ccfe1c0360190744
iasr19 <itamar.asr19@gmail.com>
2023-06-21 21:05:21 -0300

    :recycle: refresh spa

--- workspace_vscode/lab-spa/src/app/components/produto-tabela/produto-tabela.component.ts
+++ workspace_vscode/lab-spa/src/app/components/produto-tabela/produto-tabela.component.ts
@@ -11,21 +11,12 @@ export class ProdutoTabelaComponent implements OnInit {
 
   produtos: ProdutoDTO[] = [];
 
-  constructor() {
-    this.produtos = [
-      { id: 1, descricao: 'Furadeira', preco: 800.00 }
-      ,
-      { id: 2, descricao: 'Lixadeira', preco: 350.00 }
-      ,
-      { id: 3, descricao: 'Serra Circular', preco: 500.00 }
-    ];
-
-    //apenas para debug:
-    for (let p of this.produtos) {
-      console.log(p.id);
-      console.log(p.descricao);
-      console.log(p.preco);
-    }
+  constructor(private service: ProdutoService) {
+    this.service.findAll()
+      .subscribe({
+        next: (data) => this.produtos = data,
+        error: (e) => console.error(e)
+      });
   }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
